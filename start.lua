15a7a801948561533d0e49c53e294b902845b548
Fernando Paredes Garcia <fernando@develcuy.com>
2014-07-18 00:51:17 -0500

    Print debug message when something breaks at interactive.lua

--- start.lua
+++ start.lua
@@ -1,21 +1,24 @@
 #!/usr/bin/env lua5.1
 
 local irc = require 'irc'
-
 local sleep = require 'socket'.sleep
+local seawolf = require 'seawolf'.__build 'variable'
 
 local _, vault = pcall(require, 'vault')
 local s = irc.new(vault.irc)
 
 s:hook('OnChat', function(user, channel, message)
   print(('[%s] %s: %s'):format(channel, user.nick, message))
-  pcall(function ()
+  local success, err = pcall(function ()
     _G.user = user
     _G.channel = channel
     _G.message = message
     _G.irc = s
     dofile 'interactive.lua'
   end)
+  if not success then
+    seawolf.variable.print_r(err)
+  end
 end)
 
 s:connect(vault.server)
